43bbca7a2caef33971710f37318b6aebcfdab49d
hanabusasusumu <buckside1080@i.softbank.jp>
2021-03-23 20:57:13 +0900

    ユーザー登録できない時のテストコードを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,47 @@ RSpec.describe User, type: :model do
         @user.password_confirmation = '000000'
       end
     end
+
+    context '新規登録できないとき' do
+      it 'ニックネームが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'メールアドレスが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it '重複したメールアドレスが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'メールアドレスに@が含まれていないと登録できない' do
+        @user.email = 'testexample'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it 'パスワードが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'パスワード（確認用）が空では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'パスワードとパスワード（確認用）の値が一致していないと登録できない' do
+        @user.password = '000aaa'
+        @user.password_confirmation = '111aaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+    end
   end
 
 end
