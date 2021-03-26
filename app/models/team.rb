6e41a57cde9e9de8effd1e0ae421f2dc34346b02
hanabusasusumu <buckside1080@i.softbank.jp>
2021-03-26 21:38:17 +0900

    アソシエーションとバリデーションを設定

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,11 @@
 class Team < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
 
-  
+  validates :name, presence: true
+  validates :city_id, presence: true
+  validates :message, presence: true
+  validates :representative, presence: true
+  validates :emal, presence: true
+  validates :image, presence: true
 end
