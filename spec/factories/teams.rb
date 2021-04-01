b9f4c93ef4fbf1e5d09055e39438d00b12eeb1c7
hanabusasusumu <buckside1080@i.softbank.jp>
2021-04-01 22:02:59 +0900

    テスト用の情報を記述

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :team do
+    association :user
+
+    name { 'テスト' }
+    city_id { 2 }
+    message { 'テスト' }
+    representative { '山田' }
+    email { 'test@com' }
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
