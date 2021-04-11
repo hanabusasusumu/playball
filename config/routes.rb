3a4dd20ebef16ac77bcb9d2cdc04e3e1a01f2841
hanabusasusumu <buckside1080@i.softbank.jp>
2021-04-11 15:56:25 +0900

    チーム情報編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'teams/index'
   root to: 'teams#index' 
-  resources :teams, only: [:new, :create, :show]
+  resources :teams, only: [:new, :create, :show, :edit, :update]
 end
