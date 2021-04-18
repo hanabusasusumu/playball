becda730a091a9749d42e9cb409893be4362a845
hanabusasusumu <buckside1080@i.softbank.jp>
2021-04-18 15:28:48 +0900

    searchアクションを設定

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy, :show]
-
+  before_action :search_team, only: [:index, :search]
   def index
     @teams = Team.all.order('created_at DESC')
   end
@@ -46,9 +46,17 @@ class TeamsController < ApplicationController
     redirect_to root_path
   end
 
+  def search
+    @results = @t.results
+  end
+
   private
   
   def team_params
     params.require(:team).permit(:name, :city_id, :message, :representative, :email, :image).merge(user_id: current_user.id)
+  end
+
+  def search_team
+    @t = Team.ransack(params[:q])
   end
 end
